855f3905a84ce75fd36723c001ceff0e03b72e9e
KlaasHagge <klaash@ppedv.de>
2022-01-18 15:51:06 +0100

    Templates (kommentiert)

---
+++ Templates/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates
+{
+    //Bsp-Klasse
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+
+        public DateTime Datum { get; set; } = new DateTime(2003, 12, 5);
+
+        public override string ToString()
+        {
+            return Vorname;
+        }
+    }
+}
